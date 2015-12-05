567c4ae70e7bcf54fbf56e03cdc64ae775c298d7
mark sandan <msandan@utexas.edu>
2015-12-04 17:49:55 -0800

    add setup and run_test

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,9 @@
 from sqlalchemy.dialects import registry
-
-registry.register("teradata", "sqlalchemy_teradata.pyodbc", "teradataDialect_pyodbc")
-registry.register("teradata.pyodbc", "sqlalchemy_teradata.pyodbc", "teradataDialect_pyodbc")
-
 from sqlalchemy.testing import runner
+
+# registry.register("teradata", "sqlalchemy_teradata.pyodbc", "teradataDialect_pyodbc")
+registry.register("tdalchemy", "sqlalchemy_teradata.dialect", "TeradataDialect")
+# registry.register("teradata.pyodbc", "sqlalchemy_teradata.pyodbc", "teradataDialect_pyodbc")
+
 
 runner.main()
--- setup.py
+++ setup.py
@@ -7,35 +7,20 @@ v = open(os.path.join(os.path.dirname(__file__), 'sqlalchemy_teradata', '__init_
 VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
 v.close()
 
-readme = os.path.join(os.path.dirname(__file__), 'README.md')
-
-
 setup(name='sqlalchemy_teradata',
       version=VERSION,
       description="Teradata dialect for SQLAlchemy",
-      long_description=open(readme).read(),
-      classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Environment :: Console',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: Implementation :: CPython',
-      'Topic :: Database :: Front-Ends',
-      ],
       keywords='Teradata',
       author='Mike Wilson',
       author_email='mike.wilson@teradata.com',
       license='Commercial/Private',
       packages=['sqlalchemy_teradata'],
       include_package_data=True,
-      tests_require=['nose >= 0.11'],
-      test_suite="nose.collector",
+      # test_suite="nose.collector",
       zip_safe=False,
       entry_points={
          'sqlalchemy.dialects': [
-              'teradata = sqlalchemy_teradata.pyodbc:teradataDialect_pyodbc',
-              'teradata.pyodbc = sqlalchemy_teradata.pyodbc:teradataDialect_pyodbc',
+               'tdalchemy = sqlalchemy_teradata.dialect:TeradataDialect'
               ]
         }
-)
+      )
