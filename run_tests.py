ae5eb8f9e589f9bf4c376b4e5955b7bbd6c6b2c6
Mark Sandan <msandan@utexas.edu>
2016-08-16 14:05:46 -0700

    Delete run_tests.py

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.dialects import registry
-from sqlalchemy.testing import runner
-
-# registry.register("teradata", "sqlalchemy_teradata.pyodbc", "teradataDialect_pyodbc")
-registry.register("tdalchemy", "sqlalchemy_teradata.dialect", "TeradataDialect")
-# registry.register("teradata.pyodbc", "sqlalchemy_teradata.pyodbc", "teradataDialect_pyodbc")
-
-
-runner.main()
