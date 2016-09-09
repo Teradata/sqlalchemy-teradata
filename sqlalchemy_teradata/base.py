41679fa48ea72ab353de51bde15d76a4292b73bc
Mark Sandan <Mark.Sandan@Teradata.com>
2016-09-09 16:08:43 -0700

    add view DDLElements

--- sqlalchemy_teradata/base.py
+++ sqlalchemy_teradata/base.py
@@ -5,11 +5,13 @@
 # This module is part of sqlalchemy-teradata and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
+from sqlalchemy import *
 from sqlalchemy.sql import compiler
 from sqlalchemy.engine import default
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.expression import ClauseElement, Executable
 from sqlalchemy.schema import DDLElement
+from sqlalchemy.sql import table
 from sqlalchemy import types as sqltypes
 from sqlalchemy.types import CHAR, DATE, DATETIME, \
                     BLOB, CLOB, TIMESTAMP, FLOAT, BIGINT, DECIMAL, NUMERIC, \
@@ -30,18 +32,36 @@ ReservedWords = set(["abort", "abortsession", "abs", "access_lock", "account",
 class TeradataExecutionContext(default.DefaultExecutionContext):
 
     def __init__(self, dialect, connection, dbapi_connection, compiled_ddl):
-
-        super(TeradataExecutionContext, self).__init__(dialect, connection,
-                                                       dbapi_connection,
-                                                       compiled_ddl)
+        super(TeradataExecutionContext, self).__init__(dialect, connection, dbapi_connection, compiled_ddl)
 
 class TeradataIdentifierPreparer(compiler.IdentifierPreparer):
+
     reserved_words = ReservedWords
 
     def __init__(self, dialect, initial_quote='"', final_quote=None, escape_quote='"', omit_schema=False):
 
         super(TeradataIdentifierPreparer, self).__init__(dialect, initial_quote, final_quote,
                                                          escape_quote, omit_schema)
+
+# Views Recipe from: https://bitbucket.org/zzzeek/sqlalchemy/wiki/UsageRecipes/Views
+class CreateView(DDLElement):
+
+    def __init__(self, name, selectable):
+        self.name = name
+        self.selectable = selectable
+
+class DropView(DDLElement):
+
+    def __init__(self, name):
+        self.name = name
+
+@compiles(CreateView)
+def visit_create_view(element, compiler, **kw):
+    return "CREATE VIEW {} AS {}".format(element.name, compiler.sql_compiler.process(element.selectable))
+
+@compiles(DropView)
+def visit_drop_view(element, compiler, **kw):
+    return "DROP VIEW {}".format(element.name)
 
 class CreateTableAs(DDLElement):
         pass
