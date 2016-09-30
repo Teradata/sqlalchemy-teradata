56ca5d5035ae17fe0c8084d1e502d3612ac2e3b0
Mark Sandan <Mark.Sandan@Teradata.com>
2016-09-30 12:00:40 -0700

    move tests to test/

--- test/test_dialect.py
+++ test/test_dialect.py
@@ -1,8 +1,10 @@
 from sqlalchemy_teradata.dialect import TeradataDialect
 from sqlalchemy.testing import fixtures
-from sqlalchemy import testing, select
+from sqlalchemy import testing
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy_teradata.base import CreateView,DropView
+from sqlalchemy.sql import table, column, select
+from sqlalchemy import PrimaryKeyConstraint
 
 class TeradataDialectTest(fixtures.TestBase):
 
@@ -13,24 +15,26 @@ class TeradataDialectTest(fixtures.TestBase):
         self.metadata = MetaData()
 
         self.user_name = self.engine.execute('sel user').scalar()
-        self.tbl_name = self.user_name + '_test_table'
+        self.db_schema = self.engine.execute('sel database').scalar()
+        self.tbl_name = self.user_name + '_test'
         self.view_name = self.user_name + '_test_view'
 
         # Setup test table (user should have necessary rights to create table)
         self.test_table = Table(self.tbl_name, self.metadata,
-                                Column('id', Integer, primary_key=True))
+                                Column('id', Integer, primary_key=True),
+                                PrimaryKeyConstraint('id', name='my_pk'))
         # Setup a test view 
-        self.test_view = CreateView(self.view_name, select([self.test_table.c.id.label('view_id')]))
+        #self.test_view = CreateView(self.view_name, select([self.test_table.c.id.label('view_id')]))
 
         # Create tables
         self.metadata.create_all(self.engine)
 
         #Create views
-        self.conn.execute(self.test_view)
+        #self.conn.execute(self.test_view)
 
     def tearDown(self):
         # drop view(s)
-        self.conn.execute(DropView(self.view_name))
+        #self.conn.execute(DropView(self.view_name))
 
         # drop table(s)
         self.metadata.drop_all(self.engine)
@@ -48,11 +52,17 @@ class TeradataDialectTest(fixtures.TestBase):
         assert self.dialect.normalize_name(self.user_name) in schemas
 
     def test_get_view_names(self):
-        views = self.dialect.get_view_names(self.conn)
-        assert self.dialect.normalize_name(self.test_view.name) in views
+        pass
+        #views = self.dialect.get_view_names(self.conn)
+        #assert self.dialect.normalize_name(self.test_view.name) in views
 
     def test_get_pk_constraint(self):
-        assert False
+        cons = self.dialect.get_pk_constraint(self.conn, self.test_table, self.db_schema)
+        print(cons)
+        assert type(cons) is dict
+        assert self.dialect.normalize_name(cons['name']) == 'my_pk'
+        for x in cons['constrained_columns']:
+            x == self.test_table.c[x]
 
     def test_get_unique_constraint(self):
         assert False
@@ -62,3 +72,11 @@ class TeradataDialectTest(fixtures.TestBase):
 
     def test_get_indexes(self):
         assert False
+
+    def test_get_columns(self):
+        cols = self.dialect.get_columns(self.conn, self.test_table.name, self.db_schema)
+        for c in self.test_table.c:
+            assert c.name in [d['name'] for d in cols]
+
+    def test_get_transactio_mode(self):
+        assert self.dialect.get_transaction_mode(self.conn) == 'T'
