6584d054553b5e7beeada493008bf9e78c8d4ec1
RemiTurpaud <remi.turpaud@gmail.com>
2017-09-27 22:59:37 +0200

    Corrected indentation, imports

--- test/test_td_ddl.py
+++ test/test_td_ddl.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Table, Column, Index
-from sqlalchemy import CreateColumn, CreateTable, CreateIndex, CreateSchema
-from sqlalchemy import Metadata, create_engine
-from sqlalchemy_teradata import ( Varchar, Char, Clob )
-from sqlalchemy_teradata import ( Integer, Decimal, )
-from sqlalchemy_teradata import ( Date, Time, Timestamp )
+from sqlalchemy.schema import CreateColumn, CreateTable, CreateIndex, CreateSchema
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy_teradata.types import ( VARCHAR, CHAR, CLOB)
+from sqlalchemy_teradata.types import ( NUMERIC, DECIMAL, )
+#from sqlalchemy_teradata.types import ( DATE, TIME, TIMESTAMP )
 from sqlalchemy.testing import fixtures
 
 from itertools import product
@@ -19,13 +19,13 @@ class TestCompileCreateColDDL(fixtures.TestBase):
   def setup(self):
     # Test locally for now
     def dump(sql, *multiaprams, **params):
-      print(sql.compile(dialect=td_engine.dialect))
+      print(sql.compile(dialect=self.td_engine.dialect))
 
     self.td_engine = create_engine('teradata://', strategy='mock', executor=dump)
     self.sqlalch_col_attrs = ['primary_key', 'unique', 'nullable', 'default','index']
 
   def test_create_column(self):
-    c = Column('column_name', Varchar(20, charset='GRAPHIC'))
+    c = Column('column_name', VARCHAR(20, charset='GRAPHIC'))
 
   def test_col_attrs(self):
     assert False
@@ -36,15 +36,15 @@ class TestCompileCreateColDDL(fixtures.TestBase):
 
 class TestCompileCreateTableDDL(fixtures.TestBase):
 
-	def setup(self):
+    def setup(self):
         def dump(sql, *multiparams, **params):
-            print(sql.compile(dialect=td_engine.dialect))
+            print(sql.compile(dialect=self.td_engine.dialect))
         self.td_engine = create_engine('teradata://', strategy='mock', executor=dump)
 
-	def test_create_table(self):
-        meta = MetaData(bind = td_engine)
+    def test_create_table(self):
+        meta = MetaData(bind = self.td_engine)
         my_table = Table('tablename', meta,
-                        Column('column1', Integer, primary_key=True),
+                        Column('column1', NUMERIC, primary_key=True),
                         schema='database_name_or_user_name',
                         prefixes=['multiset', 'global temporary'])
 
